64939dc7533e606c390168f81c9d0214330a2359
saaddhm <saaddhm3@gmail.com>
2026-07-10 18:34:10 +0100

    mofication db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,33 @@
+// import { Sequelize } from 'sequelize';
+// import mysql2 from 'mysql2'; // <-- 1. On importe le driver directement
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST || 'localhost',
+//     port: Number(process.env.DB_PORT) || 3306,
+//     dialect: 'mysql',
+//     dialectModule: mysql2, // <-- 2. On force Sequelize à utiliser l'instance importée
+//     logging: false,
+//     define: { underscored: true },
+//     pool: { max: 10, min: 0, acquire: 30000, idle: 10000 }
+//   }
+// );
+
+// export async function connectDB() {
+//   await sequelize.authenticate();
+//   console.log('✔ MySQL connected');
+// }
+
+
 import { Sequelize } from 'sequelize';
-import mysql2 from 'mysql2'; // <-- 1. On importe le driver directement
+import mysql2 from 'mysql2';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export const sequelize = new Sequelize(
@@ -11,7 +38,8 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
-    dialectModule: mysql2, // <-- 2. On force Sequelize à utiliser l'instance importée
+    // C'est cette ligne qui force Node v24 à utiliser le package importé au lieu de le chercher dynamiquement
+    dialectModule: mysql2, 
     logging: false,
     define: { underscored: true },
     pool: { max: 10, min: 0, acquire: 30000, idle: 10000 }
